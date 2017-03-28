6be573d810698eda0b95cbfd24d03a85a2c7539f
juulderooy <juulderooy@live.nl>
2017-03-28 11:58:12 +0200

    Add files via upload

---
+++ likeapro2017/index.php
@@ -0,0 +1,24 @@
+<?php
+// ------------------------------------------------------------------------
+// Maak het onderstaande script werkend:
+// Vul de code aan en zorg dat de include-bestanden in de juiste map staan
+// ------------------------------------------------------------------------
+
+// Settigs
+include 'includes/config.php';
+
+// head section <head>
+include 'views/head.php';
+
+// hier komt de header sectie etc.
+include 'views/header.php';
+
+// hier komt de navigatie
+include 'views/menu.php';
+
+// hier komt de content van de home page
+include 'views/home.php';
+
+// hier komt de footer
+include 'views/footer.php';
+?>
